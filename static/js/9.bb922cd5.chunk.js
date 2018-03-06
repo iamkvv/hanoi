a04bd98dd92d0414b6582cc775123bdd3c8129e6
iamkvv <vkondra@gmail.com>
2018-03-06 22:03:35 +0500

    Updates

---
+++ static/js/9.bb922cd5.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([9],{67:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=t(1),a=t.n(l),s=function(){return a.a.createElement("div",null,a.a.createElement("pre",null,a.a.createElement("code",null,"const Button = ({classes, children, props}) => (\n            <button {...props} >\n                <span className={classes.label}>\n                    {() => children}\n                </span>\n            </button>")))};n.default=s}});
+//# sourceMappingURL=9.bb922cd5.chunk.js.map
